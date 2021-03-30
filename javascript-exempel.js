dd6f267e8a38bf8d05b736829cbde6dd637cb4db
Bananiver <72020028+Bananiver@users.noreply.github.com>
2021-03-30 14:21:16 +0200

    formular

---
+++ javascript-exempel.js
@@ -0,0 +1,8 @@
+function visaDialog(){
+    alert("Oliver är tillbaka");
+    changetext();
+        }
+
+        function changetext(){
+            document.getElementById("demo").innerHTML = "Oliver är tillbaka igen";
+        }
