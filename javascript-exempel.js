56f147b03ba74a9371cc1de20e15e89f21d94262
Bananiver <72020028+Bananiver@users.noreply.github.com>
2021-05-10 20:48:59 +0200

    yes

--- javascript-exempel.js
+++
@@ -1,8 +0,0 @@
-function visaDialog(){
-    alert("Oliver är tillbaka");
-    changetext();
-        }
-
-        function changetext(){
-            document.getElementById("demo").innerHTML = "Oliver är tillbaka igen";
-        }
